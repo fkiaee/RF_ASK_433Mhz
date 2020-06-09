64cff2e6414682f55cf49d4e2845a43bc221271a
fkiaee <f.kiaee@gmail.com>
2020-06-09 21:10:54 +0430

    RF ASK 433 Mhz Receiver sketch

---
+++ Receiver.ino
@@ -0,0 +1,32 @@
+/* 433 MHz RF Module Receiver Demonstration
+   Use with Transmitter Demonstration
+   Dr. Farkhondeh Kiaee
+   Instagram-Telegram: @ece_society
+*/
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library
+#include <SPI.h>
+
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+void setup()
+{
+    // Initialize ASK Object
+    rf_driver.init();
+    // Setup Serial Monitor
+    Serial.begin(9600);
+}
+void loop()
+{
+    // Set buffer to size of expected message
+    uint8_t buf[27];
+    uint8_t buflen = sizeof(buf);
+    // Check if received packet is correct size
+    if (rf_driver.recv(buf, &buflen))
+    {     
+      // Message received with valid checksum
+      Serial.print("Message Received: ");
+      Serial.println((char*)buf);        
+    }
+}
